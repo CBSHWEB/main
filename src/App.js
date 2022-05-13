eb34affbddc36d4da89f2f55bbc8867afdaea87e
Hongje Choi <enc2586@gmail.com>
2022-05-13 20:15:48 +0900

    added fonts and basic test page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Welcome, Admin!</h1>
+      <h3>폰트는 sf pro, sdgothic neo 조합이 최고입니다</h3>
+      <h3>010-1234-5678</h3>
+      <p>이거 씁시다!!</p>
     </div>
   );
 }
